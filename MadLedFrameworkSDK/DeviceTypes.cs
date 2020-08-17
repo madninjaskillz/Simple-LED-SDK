b90a6e82981576e554b9d2361c0df6cfec3a60d8
James Johnston <james@ezmuze.co.uk>
2020-08-17 01:00:54 +0100

    move to consts

--- MadLedFrameworkSDK/DeviceTypes.cs
+++ MadLedFrameworkSDK/DeviceTypes.cs
@@ -9,19 +9,23 @@ namespace MadLedFrameworkSDK
     public static class DeviceTypes
     {
         //Output Devices
-        public static string Fan = "Fan";
-        public static string LedStrip = "LED Strip";
-        public static string GPU = "GPU";
-        public static string MotherBoard = "MotherBoard";
-        public static string Keyboard = "Keyboard";
-        public static string Mouse = "Mouse";
-        public static string MousePad = "MousePad";
-        public static string Headset = "Headset";
-        public static string HeadsetStand = "HeadsetStand";
-        public static string PSU = "PSU";
-        public static string Other = "Other";
+        public const string Fan = "Fan";
+        public const string LedStrip = "LED Strip";
+        public const string GPU = "GPU";
+        public const string MotherBoard = "MotherBoard";
+        public const string Keyboard = "Keyboard";
+        public const string Mouse = "Mouse";
+        public const string MousePad = "MousePad";
+        public const string Headset = "Headset";
+        public const string HeadsetStand = "HeadsetStand";
+        public const string PSU = "PSU";
+        public const string Cooler = "Cooler";
+        public const string Memory = "Memory";
+        public const string Speaker = "Speaker";
+        public const string Bulb = "Bulb";
+        public const string Other = "Other";
 
         //Effect Sources
-        public static string Effect = "Effect";
+        public const string Effect = "Effect";
     }
 }
