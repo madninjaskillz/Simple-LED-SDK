c502630392a9c608027eca2b2e1d4edbfca2269b
James Johnston <james@ezmuze.co.uk>
2020-09-02 21:21:23 +0100

    safer device adding

--- MadLedFrameworkSDK/SLSManager.cs
+++ MadLedFrameworkSDK/SLSManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,18 @@ namespace MadLedFrameworkSDK
             List<ControlDevice> controlDevices = new List<ControlDevice>();
             foreach (var simpleLedDriver in Drivers)
             {
-                controlDevices.AddRange(simpleLedDriver.GetDevices());
+                try
+                {
+                    var devices = simpleLedDriver.GetDevices();
+                    if (devices != null)
+                    {
+                        controlDevices.AddRange(devices);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
             }
 
             return controlDevices;
